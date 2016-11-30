236f77665c5153ff32b314eaedde885161d98f75
Jerome Gravel-Niquet <jeromegn@gmail.com>
2016-11-30 13:10:26 -0500

    proper image name

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-server_image_name="861344665010.dkr.ecr.us-east-1.amazonaws.com/womrhole-remote:${BUILDKITE_COMMIT}"
+server_image_name="861344665010.dkr.ecr.us-east-1.amazonaws.com/wormhole-remote:${BUILDKITE_COMMIT}"
 
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/linux/amd64/kubectl
 chmod +x kubectl
