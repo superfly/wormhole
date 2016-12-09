b201c02ff072b7cf7d4830f1be34645f6b67ed05
Jerome Gravel-Niquet <jeromegn@gmail.com>
2016-12-09 13:47:09 -0600

    fix dpeloy [ci skip]

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-image_name="flyio/wormhole:${BUILDKITE_TAG}"
+semver=${BUILDKITE_TAG:1}
+IFS='.'; version_parts=($semver); unset IFS
+major=${version_parts[0]}
+minor=${version_parts[1]}
+patch=${version_parts[2]}
+
+image_name="flyio/wormhole:${major}.${minor}.${patch}"
 
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.4.0/bin/linux/amd64/kubectl
 chmod +x kubectl
