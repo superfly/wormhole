0501595b3c3e2fa054edefd1b18df90a6301bd97
Mat Byczkowski <mbyczkowski@gmail.com>
2017-02-09 14:15:33 -0800

    remove refs to Mat's dir path

--- scripts/wormhole-local.sh
+++ scripts/wormhole-local.sh
@@ -7,8 +7,7 @@ export PORT=8080
 
 PROTO=ssh
 WORMHOLE_BIN=$GOPATH/src/github.com/superfly/wormhole/cmd/wormhole/wormhole
-LOCAL_SERVER_CMD="go run /Users/mat/workspace/golang/src/github.com/valyala/fasthttp/examples/fileserver/fileserver.go -addr localhost:$PORT -dir ~/Downloads/"
-#LOCAL_SERVER_CMD="cd /Users/mat/workspace/ruby-getting-started && bundle exec puma -C config/puma.rb -p $PORT"
+LOCAL_SERVER_CMD="go run $GOPATH/src/github.com/valyala/fasthttp/examples/fileserver/fileserver.go -addr localhost:$PORT -dir ."
 
 redis-cli HGET backend_tokens $FLY_TOKEN
 
