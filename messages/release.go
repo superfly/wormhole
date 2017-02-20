af74e5437130a38bec694a02b1113a27cf218186
Mat Byczkowski <mbyczkowski@gmail.com>
2017-02-20 14:15:28 -0800

    omit empty release fields

--- messages/release.go
+++ messages/release.go
@@ -4,13 +4,13 @@ import "time"
 
 // Release ...
 type Release struct {
-	ID                     string    `redis:"id"`
-	Branch                 string    `redis:"branch"`
-	Description            string    `redis:"description"`
-	VCSType                string    `redis:"vcs_type"`
-	VCSRevision            string    `redis:"vcs_revision"`
-	VCSRevisionMessage     string    `redis:"vcs_revision_message"`
-	VCSRevisionTime        time.Time `redis:"vcs_revision_time"`
-	VCSRevisionAuthorName  string    `redis:"vcs_revision_author_name"`
-	VCSRevisionAuthorEmail string    `redis:"vcs_revision_author_email"`
+	ID                     string    `redis:"id,omitempty"`
+	Branch                 string    `redis:"branch,omitempty"`
+	Description            string    `redis:"description,omitempty"`
+	VCSType                string    `redis:"vcs_type,omitempty"`
+	VCSRevision            string    `redis:"vcs_revision,omitempty"`
+	VCSRevisionMessage     string    `redis:"vcs_revision_message,omitempty"`
+	VCSRevisionTime        time.Time `redis:"vcs_revision_time,omitempty"`
+	VCSRevisionAuthorName  string    `redis:"vcs_revision_author_name,omitempty"`
+	VCSRevisionAuthorEmail string    `redis:"vcs_revision_author_email,omitempty"`
 }
