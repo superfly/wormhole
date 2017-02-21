8e53e01d09eaf34edb9f2b3c85b0187379cd6c2a
Mat Byczkowski <mbyczkowski@gmail.com>
2017-02-21 11:14:27 -0800

    bring ARM target for Linux

--- .buildkite/compile.sh
+++ .buildkite/compile.sh
@@ -19,15 +19,19 @@ GCFLAGS=""
 rm -rf pkg && mkdir -p pkg
 
 OSES=(linux darwin windows freebsd)
-ARCHS=(amd64 386)
+ARCHS=(amd64 386 arm)
 for os in ${OSES[@]}; do
 	for arch in ${ARCHS[@]}; do
 		suffix=""
-		if [ "$os" == "windows" ]
-		then
+		if [ "$os" == "windows" ]; then
 			suffix=".exe"
 		fi
+
+		# only compile ARM target for linux
+		if [ "$arch" == "arm" ] && [ "$os" != "linux" ]; then
+			continue
+		fi
 		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o pkg/wormhole_${os}_${arch}${suffix} github.com/superfly/wormhole/cmd/wormhole
-    $MD5 pkg/wormhole_${os}_${arch}${suffix}
+		$MD5 pkg/wormhole_${os}_${arch}${suffix}
 	done
 done
