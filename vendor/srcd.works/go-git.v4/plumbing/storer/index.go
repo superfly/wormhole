c6c6e3e644e25d878999817744339db3bcf9e87b
Mat Byczkowski <mbyczkowski@gmail.com>
2017-04-26 11:52:11 -0700

    stop tracking vendorized dependencies

--- vendor/srcd.works/go-git.v4/plumbing/storer/index.go
+++
@@ -1,9 +0,0 @@
-package storer
-
-import "srcd.works/go-git.v4/plumbing/format/index"
-
-// IndexStorer generic storage of index.Index
-type IndexStorer interface {
-	SetIndex(*index.Index) error
-	Index() (*index.Index, error)
-}
