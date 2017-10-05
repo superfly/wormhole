921535428777f37d03e5d451ffef6efae8c76a81
Mat Byczkowski <mbyczkowski@gmail.com>
2017-10-05 16:48:55 -0700

    don't need buildkite anymore

--- .buildkite/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker run --rm \
-  -v $(pwd):/go/src/github.com/superfly/wormhole:rw \
-  --entrypoint /go/src/github.com/superfly/wormhole/.buildkite/test_entrypoint.sh \
-  --workdir /go/src/github.com/superfly/wormhole \
-  golang
