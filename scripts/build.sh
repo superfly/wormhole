db7d8321babc1ebc277e001fe82270a3405df8e9
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-06-20 15:55:43 -0400

    logging more s3 stuff to fix build upload issue

--- scripts/build.sh
+++ scripts/build.sh
@@ -123,8 +123,11 @@ go get -u github.com/minio/mc
 
 mc config host add s3 https://s3.amazonaws.com $AWS_S3_ACCESS_KEY_ID $AWS_S3_SECRET_ACCESS_KEY
 
+echo "Pushing to s3/flyio-wormhole-builds/$VERSION/"
+
 mc -q cp ./pkg/wormhole* s3/flyio-wormhole-builds/$VERSION/
 
+echo "Pushing to s3/flyio-wormhole-builds/$CHANNEL/"
 # also set the version as the latest
 # TODO: there must be a better way to copy/symlink objects in S3 instead of uploading again
 mc -q cp ./pkg/wormhole* s3/flyio-wormhole-builds/$CHANNEL/
